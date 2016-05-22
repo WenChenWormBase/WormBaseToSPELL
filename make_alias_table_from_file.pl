90e4c0c5f23189494b8d431e2dcd17235c5efdfb
Wen Chen <wen@wormbase.org>
2016-05-22 14:54:49 -0700

    include Status column in WBGeneName.csv file

--- make_alias_table_from_file.pl
+++ make_alias_table_from_file.pl
@@ -1,44 +1,143 @@
 #!/usr/bin/perl
 
 #-------------------Type out the purpose of the script-------------
-print "This program check WS Gene_names and make alias file for SPELL\n";
-print "Input file: /home/wen/WormBaseToSPELL/ace_files/WBGeneName.ace\n";
-print "Output file: alias_to_systematic.txt, gene_list.txt, systematic_to_common.txt\n\n";
+print "This program check WS Gene_names and make alias file for SPELL, also create Gene Name a table for download.\n";
+print "Input file: /home/wen/WormBaseToSPELL/ace_files/WBGeneIdentity.ace\n";
+print "Output file: alias_to_systematic.txt, gene_list.txt, systematic_to_common.txt, WBGeneName.csv\n\n";
 
 
 print "Parse Gene names ...";
-open (IN, "/home/wen/WormBaseToSPELL/ace_files/WBGeneName.ace") || die "can't open $!";
-open (ALIAS, ">alias_to_systematic.txt") || die "can't open $!";
-open (GENES, ">gene_list.txt") || die "can't open $!";
-open (COMMON, ">systematic_to_common.txt") || die "can't open $!";
+open (IN, "/home/wen/WormBaseToSPELL/ace_files/WBGeneIdentity.ace") || die "can't open WBGeneIdentity.ace!";
 
+open (ALIAS, ">alias_to_systematic.txt") || die "can't open alias_to_systematic.txt!";
+open (GENES, ">gene_list.txt") || die "can't open gene_list.txt!";
+open (COMMON, ">systematic_to_common.txt") || die "can't open systematic_to_common.txt!";
+open (OUT, ">WBGeneName.csv") || die "cannot open WBGeneName.csv!\n";
+
+print OUT "Gene\tPublic Name\tStatus\tSequence Name\tUniprot\tTreeFam\tRefSeq_mRNA\tRefSeq_protein\n";
+my ($line, $g, $pub_name, $status, $seq_name, $uniprot, $treefam, $refSeqRNA, $refSeqProtein, $other_name, $mol_name);
+my @tmp;
+my %otherName;
+my %pubName;
+my @otherNameList;
+#my %idGene;
 
 $id = 0;
 $id_a = 0;
 $id_c = 0;
+$oth = 0;
 
-while ($Line=<IN>) {
-    chomp ($Line);
-    if ($Line =~ /^Gene : /) {
-	($stuff1, $GeneID, $stuff2) = split ('"', $Line);	
+$line = <IN>;
+while ($line =<IN>) {
+    chomp($line);
+    if ($line =~ /^Gene/) {
+	@tmp = split '"', $line;
+	$g = $tmp[1];
+	$pub_name = "N.A.";
+	$seq_name = "N.A.";
+	$uniprot = "N.A.";
+	$treefam = "N.A.";
+	$refSeqRNA = "N.A.";
+	$refSeqProtein = "N.A.";
+
 	$id++;
-	print GENES "$id\t$GeneID\n";
-    } elsif ($Line =~ /^Public_name/) {
-	($stuff1, $Public_name, $stuff2) = split ('"', $Line);
+	print GENES "$id\t$g\n";
+	$idGene{$g} = $id;
+    } elsif ($line =~ /^Live/) {
+	$status = "Live";
+    } elsif ($line =~ /^Dead/) {
+	$status = "Dead";
+    } elsif ($line =~ /^Sequence_name/) {
+	@tmp = split '"', $line;
+	$seq_name = $tmp[1];
+
 	$id_a++;
-	print ALIAS "$id_a\t$Public_name\t$GeneID\n";
+	print ALIAS "$id_a\t$seq_name\t$g\n";
+
+    } elsif ($line =~ /^Molecular_name/) {
+	@tmp = split '"', $line;
+	$mol_name = $tmp[1];
+
+	$id_a++;
+	print ALIAS "$id_a\t$mol_name\t$g\n";
+
+     } elsif ($line =~ /^Other_name/) {
+	@tmp = split '"', $line;
+	$other_name = $tmp[1];
+	$otherName{$other_name} = $g;
+	$otherNameList[$oth] = $other_name;
+	$oth++;
+    } elsif ($line =~ /^Public_name/) {
+	@tmp = split '"', $line;
+	$pub_name = $tmp[1];
+	$pubName{$pub_name} = $g;
+
+	$id_a++;
+	print ALIAS "$id_a\t$pub_name\t$g\n";
 	$id_c++;
-	print COMMON "$id_c\t$GeneID\t$Public_name\n";
-    } elsif (($Line =~ /^CGC_name/) || ($Line =~ /^Sequence_name/) || ($Line =~ /^Molecular_name/) || ($Line =~ /^Other_name/)) {
-	($stuff1, $Other_name, $stuff2) = split ('"', $Line);
+	print COMMON "$id_c\t$g\t$pub_name\n";
+    }  elsif ($line =~ /UniProt/) {
+	@tmp = split '"', $line;
+
 	$id_a++;
-	print ALIAS "$id_a\t$Other_name\t$GeneID\n";
+	print ALIAS "$id_a\t$tmp[5]\t$g\n";
+
+	if ($uniprot eq "N.A.") {
+	    $uniprot = $tmp[5];
+	} else {
+	    $uniprot = join ",",  $uniprot, $tmp[5];
+	}
+
+    }  elsif ($line =~ /TREEFAM/) {
+	@tmp = split '"', $line;
+
+	$id_a++;
+	print ALIAS "$id_a\t$tmp[5]\t$g\n";
+
+	$treefam = $tmp[5];
+    }  elsif (($line =~ /RefSeq/) && ($line =~ /mRNA/)) {
+	@tmp = split '"', $line;
+
+	$id_a++;
+	print ALIAS "$id_a\t$tmp[5]\t$g\n";
+	
+	if ($refSeqRNA eq "N.A.") {
+	    $refSeqRNA = $tmp[5];
+	} else {
+	    $refSeqRNA = join ",",  $refSeqRNA, $tmp[5];
+	}
+    }  elsif (($line =~ /RefSeq/) && ($line =~ /protein/)) {
+	@tmp = split '"', $line;
+
+	$id_a++;
+	print ALIAS "$id_a\t$tmp[5]\t$g\n";
+
+	if ($refSeqProtein eq "N.A.") {
+	    $refSeqProtein = $tmp[5];
+	} else {
+	    $refSeqProtein = join ",",  $refSeqProtein, $tmp[5];
+	}
+    } elsif  ($line eq "") {
+	print OUT "$g\t$pub_name\t$status\t$seq_name\t$uniprot\t$treefam\t$refSeqRNA\t$refSeqProtein\n";
     }
 }
+
+foreach $other_name (@otherNameList) {
+    if ($pubName{$other_name}) {
+	#print "$other_name was already used as a public name for $pubName{$other_name}\n"; 
+    } else {
+	if ($otherName{$other_name}) {
+	    $id_a++;
+	    print ALIAS "$id_a\t$other_name\t$otherName{$other_name}\n";
+	}
+    }
+}
+
 close (IN);
 close (ALIAS);
 close (GENES);
 close (COMMON);
+close (OUT);
 print "Done.\n";
 
 
