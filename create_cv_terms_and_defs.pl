c34e4426d6017abf5bccf5cdfd3673a61ac2ef66
Wen Chen <wen@wormbase.org>
2021-07-23 16:39:26 -0700

    Swtich to use SPELLDataSet_Topics_annotation.csv

--- create_cv_terms_and_defs.pl
+++ create_cv_terms_and_defs.pl
@@ -1,39 +1,13 @@
 #!/usr/bin/perl
 use strict;
-#use Ace;
-
-#print "Look for Topics ...\n";
-#my $db = Ace->connect(-path => '/home/citace/WS/acedb/',  -program => '/usr/local/bin/tace') || die print "Connection failure: ", Ace->error;
-
-#my $query="QUERY FIND WBProcess Public_name =*";
-
-#my @topic = $db->find($query);
-#my %topicName;
-
-#open (CV, ">add_CV_link.rhtml") || die "can't open $!";
-#foreach my $t (@topic) {
-#    my $name = $t->Public_name;
-#    my $def = $t->Summary;
-#    #$topicName{$t} = $name;
-#    print CV " \<tr height=13\>\n";
-#    print CV "  \<td height=13\>\<a href\=\"http:\/\/www.wormbase.org\/resources\/wbprocess\/$t\"\>Topic: $name<\/a\><\/td>\n";
-#    print CV "  \t\<td\>$def\n";
-#    print CV "  \t\<\/td\>\n";
-#    print CV " \<\/tr\>\n\n";
-#}
-#print "done.\n";
-#$db->close();
-#close(CV);
-
 
 my %defTopic;
 my ($line, $go, $def, $topic);
 my @tmp;
+my @topicList;
+my @sortedTopicList;
 
-
-open (CV, ">add_CV_link.rhtml") || die "can't open add_CV_link.rhtml!";
-
-open (GO, "/home/citace/WS/ONTOLOGY/gene_ontology.WS268.obo") || die "can't opengene_ontology.WS268.obo!";
+open (GO, "/home/wen/SPELL/TablesForSPELL/gene_ontology.obo") || die "can't open gene_ontology.obo!";
 while ($line = <GO>) {
 
     chomp($line);
@@ -50,10 +24,17 @@ close (GO);
 
 open (IN, "/home/wen/WormBaseToSPELL/currentSPELL/topicList.csv") || die "can't open topicList.csv!";
 $line = <IN>;
+my $i = 0;
 while ($line = <IN>) {
-
     chomp($line);
- 
+    $topicList[$i] = $line;
+    $i++;
+}
+@sortedTopicList = sort { lc($a) cmp lc($b) } @topicList;
+close (IN);
+
+open (CV, ">add_CV_link.rhtml") || die "can't open add_CV_link.rhtml!";
+foreach $line (@sortedTopicList) {
     if ($line =~ /GO:/) {
 	@tmp = split / \| /, $line;
 	$go = $tmp[1];
@@ -75,15 +56,6 @@ while ($line = <IN>) {
     print CV "  \t\<td\>$def\n";
     print CV "  \t\<\/td\>\n";
     print CV " \<\/tr\>\n\n";
-
 }
-
 close (CV);
-close (GO);
-close (IN);
-
-
-
-
-
-
+print "$i SPELL topics are used.\n"
